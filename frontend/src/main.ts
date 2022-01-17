872319877e5208abca1ba665a881ba162ce17688
monkhaus <tom.monkhouse95@gmail.com>
2022-01-17 13:36:36 +0000

    add bulma, axios + store & main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,5 +3,9 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import axios from 'axios'
 
-createApp(App).use(store).use(router).mount('#app');
+// axios.defaults.baseURL = 'https://api.youtubeideagenerator.com/'
+axios.defaults.baseURL = 'http://0.0.0.0:8000'
+
+createApp(App).use(store).use(router, axios).mount('#app');
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,8 +2,29 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    token: '',
+    isAuthenticated: false,
+    activeUser: '',
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem('token')) {
+        state.token = String(localStorage.getItem('token'))
+        state.isAuthenticated = true
+      } else {
+        state.token = ""
+        state.isAuthenticated = false
+      }
+    },
+    // token is set when user signs in
+    setToken(state, token) {
+      state.token = token
+      state.isAuthenticated = true
+    },
+    removeToken(state) {
+      state.token = ''
+      state.isAuthenticated = false
+    }
   },
   actions: {
   },
