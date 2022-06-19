c7fa2ee232c750b398fe5cb4fb5cfb4e322a2f7e
monkhaus <tom.monkhouse95@gmail.com>
2022-06-19 17:47:22 +0100

    add: display model fields in admin for extendedusermodel

--- api/admin.py
+++ api/admin.py
@@ -5,10 +5,13 @@ from api.models.extended_user import ExtendedUserModel
 
 
 class ChannelAdmin(admin.ModelAdmin):
-    list_display = ('channel_name', 'channel_id', 'channel_url', 'channel_logo')
+    list_display = ('channel_name', 'channel_id', 'channel_url', 'channel_logo', 'channel_subscribers')
+
+class ExtendedUserModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'isPremium')
 
 # Register your models here.
 admin.site.register(Channel, ChannelAdmin)
 admin.site.register(UserSyncedChannel)
 admin.site.register(Video)
-admin.site.register(ExtendedUserModel)
+admin.site.register(ExtendedUserModel, ExtendedUserModelAdmin)
